86909cdcfb2a9ff7939eca8bab5974888740cae8
Giovanni Marchiori <giovanni.marchiori@cern.ch>
2024-03-27 17:19:34 +0100

    prepare for addition of args to printCutFlow

--- case-studies/higgs/hcc/analysis/ZllHqq/analysis-stage1-interactive.sh
+++ case-studies/higgs/hcc/analysis/ZllHqq/analysis-stage1-interactive.sh
@@ -5,17 +5,27 @@
 analysis_config=${FCCANACONFS}
 analysis_script=analysis_stage1.py
 
+campaign=winter2023
 detector=IDEA
 
 # input files (from Pythia+Delphes)
-path=/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/${detector}
+# path=/eos/experiment/fcc/ee/generation/DelphesEvents/winter2023/${detector}
+path=/eos/user/g/gmarchio/fcc/generation/DelphesEvents/${campaign}/${detector}
 
 # output path
 # outputpath=/eos/user/g/gmarchio/fcc-new/ZllHqq/analysis/root/${detector}
 outputpath=.
 
 for process in \
-    wzp6_ee_eeH_Hbb_ecm240 \
+    wzp6_ee_eeH_Hbs_ecm240 \
+    wzp6_ee_eeH_Hbd_ecm240 \
+    wzp6_ee_eeH_Hsd_ecm240 \
+    wzp6_ee_eeH_Hcu_ecm240 \
+    wzp6_ee_mumuH_Hbs_ecm240 \
+    wzp6_ee_mumuH_Hbd_ecm240 \
+    wzp6_ee_mumuH_Hsd_ecm240 \
+    wzp6_ee_mumuH_Hcu_ecm240 \
+#    wzp6_ee_eeH_Hbb_ecm240 \
 #    wzp6_ee_eeH_HZZ_ecm240 
 #    wzp6_ee_mumuH_Hgg_ecm240 
 #    wzp6_ee_eeH_Hcc_ecm240 \
@@ -49,7 +59,7 @@ for process in \
 do
     echo "Running over files in $path/$process"
 # everything but nonhad
-    fccanalysis run $analysis --output ${outputpath}/${process}.root --files-list ${path}/${process}/events_*.root
+    fccanalysis run $analysis_config/$analysis_script --output ${outputpath}/${process}.root --files-list ${path}/${process}/events_*.root
 # debug 1000 events
     # fccanalysis run $analysis_config/$analysis_script --output ${outputpath}/${process}.root --files-list ${path}/${process}/events_*.root  --nevents 1000
 # debug 1 file (test MT)
--- case-studies/higgs/hcc/analysis/ZllHqq/printCutflow.py
+++ case-studies/higgs/hcc/analysis/ZllHqq/printCutflow.py
@@ -6,8 +6,9 @@ from analysis_config import *
 
 import subprocess
 import math
+import argparse
 
-debug = False
+debug = True
 
 # Z(ll)+jets sample is Pythia8 Z(ll) (False) or WzPy6 ee+mumu (True)
 splitZllByFlavour = True
@@ -82,147 +83,151 @@ cutList['finalsel_mu'] = 'l=mu'
 basedir += 'analysis-final/hists/'
 
 
-yieldsInitial = {}
-yieldsFinal = {}
-yieldsFinal_e = {}
-yieldsFinal_mu = {}
-yieldsPrevious = {}
-
-# print title of table
-print('')
-if printSig:
-    print('{:25s} '.format('Cut'), end='')
-    for proc in processes:
-        if 'ZH' in proc:
-            print('{:>10s} {:>5s} '.format(proc, ''), end='')
-        else:
-            print('{:>10s} '.format(proc), end='')
+def main():
+    yieldsInitial = {}
+    yieldsFinal = {}
+    yieldsFinal_e = {}
+    yieldsFinal_mu = {}
+    yieldsPrevious = {}
+    
+    # print title of table
     print('')
-    print('{:25s} '.format(''), end='')
-    for proc in processes:
-        if 'ZH' in proc:
-            print('{:>10s} {:>5s} '.format('Yield', 'Sig'), end='')
-        else:
-            print('{:>10s} '.format('Yield'), end='')
-    print('')              
-else:
-    print('{:25s} '.format('Cut'), end='')
-    for proc in processes:
-        print('{:>10s} {:>5s} '.format(proc, ''), end='')
-    print('')
-    print('{:25s} '.format(''), end='')
-    for proc in processes:
-        print('{:>10s} {:>5s} '.format('Yield', 'Eff'), end='')
-    print('')               
-
-# loop over the cuts and populate table rows
-for cut in cutList:
-    if debug: print(cut)
-    yields = {}
-    sumYields = 0.0
-    for proc in processes:
-        yields[proc] = 0.
-        for subproc in processes[proc]:
-            filename = basedir+'/'+subproc+'_cutflow_weighted.txt'
-            if os.path.isfile(filename):
-                cmd = "grep \ " + cut + "\  " + basedir + "/"+subproc+"_cutflow_weighted.txt | awk '{print $5}'"
-                if debug: print(cmd)
-                subproc_return = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE).stdout.read().decode()
-                if debug: print(subproc_return)
-                yields[proc] += float(subproc_return.split('\n')[-2])
-        else:
-            yields[proc] += 0.0
-        sumYields += yields[proc]
-    if (cut=='selNone'): yieldsInitial=dict(yields)
-    if (cut=='finalsel'): yieldsFinal=dict(yields)
-    #if (cut=='selN_nn'): yieldsFinal=dict(yields)
-    #if (cut=='selN_lepveto'): yieldsFinal=dict(yields)
-    if (cut=='finalsel_e'): yieldsFinal_e=dict(yields)
-    if (cut=='finalsel_mu'): yieldsFinal_mu = dict(yields)
-    print('{:25s} '.format(cutList[cut]), end='')
     if printSig:
-        sig = {}
+        print('{:25s} '.format('Cut'), end='')
         for proc in processes:
             if 'ZH' in proc:
-                sig[proc] = yields[proc]/math.sqrt(sumYields)
-                print('{:10.0f} {:5.0f} '.format(yields[proc],sig[proc]), end='')
+                print('{:>10s} {:>5s} '.format(proc, ''), end='')
             else:
-                print('{:10.0f} '.format(yields[proc]), end='')
+                print('{:>10s} '.format(proc), end='')
         print('')
+        print('{:25s} '.format(''), end='')
+        for proc in processes:
+            if 'ZH' in proc:
+                print('{:>10s} {:>5s} '.format('Yield', 'Sig'), end='')
+            else:
+                print('{:>10s} '.format('Yield'), end='')
+        print('')              
     else:
-        eff = {}        
-        if cut!='selNone' and cut!='finalsel_e' and cut!='finalsel_mu':
+        print('{:25s} '.format('Cut'), end='')
+        for proc in processes:
+            print('{:>10s} {:>5s} '.format(proc, ''), end='')
+        print('')
+        print('{:25s} '.format(''), end='')
+        for proc in processes:
+            print('{:>10s} {:>5s} '.format('Yield', 'Eff'), end='')
+        print('')               
+
+    # loop over the cuts and populate table rows
+    for cut in cutList:
+        if debug: print(cut)
+        yields = {}
+        sumYields = 0.0
+        for proc in processes:
+            yields[proc] = 0.
+            for subproc in processes[proc]:
+                filename = basedir+'/'+subproc+'_cutflow_weighted.txt'
+                if os.path.isfile(filename):
+                    cmd = "grep \ " + cut + "\  " + basedir + "/"+subproc+"_cutflow_weighted.txt | awk '{print $5}'"
+                    if debug: print(cmd)
+                    subproc_return = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE).stdout.read().decode()
+                    if debug: print(subproc_return)
+                    yields[proc] += float(subproc_return.split('\n')[-2])
+            else:
+                yields[proc] += 0.0
+            sumYields += yields[proc]
+        if (cut=='selNone'): yieldsInitial=dict(yields)
+        if (cut=='finalsel'): yieldsFinal=dict(yields)
+        #if (cut=='selN_nn'): yieldsFinal=dict(yields)
+        #if (cut=='selN_lepveto'): yieldsFinal=dict(yields)
+        if (cut=='finalsel_e'): yieldsFinal_e=dict(yields)
+        if (cut=='finalsel_mu'): yieldsFinal_mu = dict(yields)
+        print('{:25s} '.format(cutList[cut]), end='')
+        if printSig:
+            sig = {}
             for proc in processes:
-                eff[proc] = 100.*yields[proc]/yieldsPrevious[proc]
-                print('{:10.0f} {:5.0f} '.format(yields[proc], eff[proc]), end='')
+                if 'ZH' in proc:
+                    sig[proc] = yields[proc]/math.sqrt(sumYields)
+                    print('{:10.0f} {:5.0f} '.format(yields[proc],sig[proc]), end='')
+                else:
+                    print('{:10.0f} '.format(yields[proc]), end='')
+            print('')
         else:
-            for proc in processes:
-                print('{:10.0f} {:>5s} '.format(yields[proc], '-'), end= '')
-        print('')
-        yieldsPrevious = dict(yields)  
-print('\n')
+            eff = {}        
+            if cut!='selNone' and cut!='finalsel_e' and cut!='finalsel_mu':
+                for proc in processes:
+                    eff[proc] = 100.*yields[proc]/yieldsPrevious[proc]
+                    print('{:10.0f} {:5.0f} '.format(yields[proc], eff[proc]), end='')
+            else:
+                for proc in processes:
+                    print('{:10.0f} {:>5s} '.format(yields[proc], '-'), end= '')
+            print('')
+            yieldsPrevious = dict(yields)  
+    print('\n')
 
-str = '{:25s}'.format('Efficiency (%)')
-for proc in processes: str+='{:>10s}'.format(proc)
-print(str)
-str = '{:25s}'.format('')
-for proc in processes:
-    if (yieldsInitial[proc]!=0.):
-        str+='{:10.2f}'.format(yieldsFinal[proc]*100./yieldsInitial[proc])
-    else:
-        str+='{:10s}'.format('')
-print(str)
-print('')
-
-
-# Print efficiency separately for ee and mumu channels
-str = '{:25s}'.format('Eff. in e channel (%)')
-for process in processes: str+='{:>10s}'.format(process)
-print(str)
-str = '{:25s}'.format('')
-for proc in processes:
-    if (yieldsInitial[proc]!=0.):
-        str+='{:10.2f}'.format(yieldsFinal_e[proc]*200./yieldsInitial[proc])
-    else:
-        str+='{:10s}'.format('')
-print(str)
-print('')
-
-str = '{:25s}'.format('Eff. in mu channel (%)')
-for process in processes: str+='{:>10s}'.format(process)
-print(str)
-str = '{:25s}'.format('')
-for proc in processes: 
-    if (yieldsInitial[proc]!=0.):
-        str+='{:10.2f}'.format(yieldsFinal_mu[proc]*200./yieldsInitial[proc])
-    else:
-        str+='{:10s}'.format('')
-print(str)
-print('')
-
-
-# Print efficiency for HWW/ZZ/tautau normalised taking into account hadronic BRs
-# Not correct because some events pass selection even if they are not fully had
-# For instance Z(nunu)Z(qq)
-if splitZHother:
-    BRhad = {
-        'WW': 0.454,
-        'ZZ': 0.489,
-        'tautau': 0.42
-    }
-    # calculated from mumuH_HWW/ZZ/tautau after finalsel
-    fracHad = {
-        'WW': 0.62539938,
-        'ZZ': 0.53700247,
-        'tautau': 0.56990203
-        }
-    str = '{:50s}'.format('Eff. in ZH(other) channels wrt had decays (%)')
-    for decay in BRhad:
-        str+='{:>10s}'.format(decay)
+    str = '{:25s}'.format('Efficiency (%)')
+    for proc in processes: str+='{:>10s}'.format(proc)
     print(str)
-    str = '{:50s}'.format('')
-    for decay in BRhad:
-        str+='{:10.2f}'.format(yieldsFinal['ZH'+decay]*100./yieldsInitial['ZH'+decay]*fracHad[decay]/BRhad[decay])  
+    str = '{:25s}'.format('')
+    for proc in processes:
+        if (yieldsInitial[proc]!=0.):
+            str+='{:10.2f}'.format(yieldsFinal[proc]*100./yieldsInitial[proc])
+        else:
+            str+='{:10s}'.format('')
     print(str)
     print('')
 
+
+    # Print efficiency separately for ee and mumu channels
+    str = '{:25s}'.format('Eff. in e channel (%)')
+    for process in processes: str+='{:>10s}'.format(process)
+    print(str)
+    str = '{:25s}'.format('')
+    for proc in processes:
+        if (yieldsInitial[proc]!=0.):
+            str+='{:10.2f}'.format(yieldsFinal_e[proc]*200./yieldsInitial[proc])
+        else:
+            str+='{:10s}'.format('')
+    print(str)
+    print('')
+
+    str = '{:25s}'.format('Eff. in mu channel (%)')
+    for process in processes: str+='{:>10s}'.format(process)
+    print(str)
+    str = '{:25s}'.format('')
+    for proc in processes: 
+        if (yieldsInitial[proc]!=0.):
+            str+='{:10.2f}'.format(yieldsFinal_mu[proc]*200./yieldsInitial[proc])
+        else:
+            str+='{:10s}'.format('')
+    print(str)
+    print('')
+
+
+    # Print efficiency for HWW/ZZ/tautau normalised taking into account hadronic BRs
+    # Not correct because some events pass selection even if they are not fully had
+    # For instance Z(nunu)Z(qq)
+    if splitZHother:
+        BRhad = {
+            'WW': 0.454,
+            'ZZ': 0.489,
+            'tautau': 0.42
+        }
+        # calculated from mumuH_HWW/ZZ/tautau after finalsel
+        fracHad = {
+            'WW': 0.62539938,
+            'ZZ': 0.53700247,
+            'tautau': 0.56990203
+            }
+        str = '{:50s}'.format('Eff. in ZH(other) channels wrt had decays (%)')
+        for decay in BRhad:
+            str+='{:>10s}'.format(decay)
+        print(str)
+        str = '{:50s}'.format('')
+        for decay in BRhad:
+            str+='{:10.2f}'.format(yieldsFinal['ZH'+decay]*100./yieldsInitial['ZH'+decay]*fracHad[decay]/BRhad[decay])  
+        print(str)
+        print('')
+
+
+if __name__ == '__main__':
+    main()
