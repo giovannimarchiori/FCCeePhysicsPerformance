4d3461ee1c42a08ba191518cfdbcd9e5183cb7b0
Giovanni Marchiori <giovanni.marchiori@cern.ch>
2024-04-04 12:06:42 +0200

    add script to check condor errors

---
+++ case-studies/higgs/hcc/analysis/ZllHqq/condor/checkCondorErrors.py
@@ -0,0 +1,13 @@
+import subprocess
+#result = subprocess.run(["ls -l std/*.err | awk '$5>1000'"], stdout=subprocess.PIPE).stdout.decode()
+result = subprocess.check_output("ls -l std/*.err | awk '$5>1000'", shell=True).decode().split("\n")
+for line in result:
+    if (line != ""):
+        err = line.split()[-1]
+        print("Possible error in file", err)
+        log = err[:-4]+".out"
+        rootfile = subprocess.run(["grep", "events_", log], stdout=subprocess.PIPE).stdout.decode().split()[2][:-1]
+        sample = rootfile.split("/")[-2]
+        print("-> sample:", sample)
+        print("-> input file:", rootfile)
+        print()
