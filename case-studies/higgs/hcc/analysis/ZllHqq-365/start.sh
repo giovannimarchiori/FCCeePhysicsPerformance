78779f96d98f6b2e669db96a0d89c1dd8f7324ea
Giovanni Marchiori <giovanni.marchiori@cern.ch>
2024-05-24 12:25:58 +0200

    update for 365 GeV

--- case-studies/higgs/hcc/analysis/ZllHqq-365/start.sh
+++ case-studies/higgs/hcc/analysis/ZllHqq-365/start.sh
@@ -1,6 +1,10 @@
 os=`cat /etc/os-release | grep ^ID= | awk -F '"' '{print $2}'`
 if [[ "$os" == "rocky" ]]; then
+    echo "OS is rocky"
     source setup_rocky.sh
+    export FCCANAOUTPUT=$PWD/../../../../../../output/ZllHqq-365/
+else
+    export FCCANAOUTPUT=/eos/user/g/gmarchio/fcc/analysis/selection/ZllHqq-365/
 fi
 export FCCANALYSES=$PWD/../../../../../../FCCAnalyses
 export FCCANALYSIS=$PWD
@@ -10,8 +14,5 @@ cd $FCCANALYSES
 source setup.sh
 cd $cwd
 export FCCANALYSES=$PWD/../../../../../../FCCAnalyses
-if [[ "$os" == "rocky" ]]; then
-    export FCCANAOUTPUT=
-else
-    export FCCANAOUTPUT=/eos/user/g/gmarchio/fcc/analysis/selection/ZllHqq-365/
-fi
+
+
