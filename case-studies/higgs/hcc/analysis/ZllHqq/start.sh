6c137edc0929e9419a53d6e78129ea69c939377c
Giovanni Marchiori <giovanni.marchiori@cern.ch>
2024-10-18 09:57:38 +0200

    update

--- case-studies/higgs/hcc/analysis/ZllHqq/start.sh
+++ case-studies/higgs/hcc/analysis/ZllHqq/start.sh
@@ -1,10 +1,18 @@
 os=`cat /etc/os-release | grep ^ID= | awk -F '"' '{print $2}'`
 if [[ "$os" == "rocky" ]]; then
+    echo "OS is rocky"
     source setup_rocky.sh
+    export FCCANAOUTPUT=$PWD/../../../../../../output/ZllHqq/
+else
+    export FCCANAOUTPUT=/eos/user/g/gmarchio/fcc/analysis/selection/ZllHqq/
 fi
+export FCCANALYSES=$PWD/../../../../../../FCCAnalyses
+export FCCANALYSIS=$PWD
+export FCCANACONFS=$PWD/../../FCCAnalyses-config/ZllHqq
 cwd=$PWD
-cd $FCCANALYSESPKG
+cd $FCCANALYSES
 source setup.sh
 cd $cwd
+export FCCANALYSES=$PWD/../../../../../../FCCAnalyses
 
 
